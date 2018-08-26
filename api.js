a9a9b2ce45b4cf66903f95f3c1adc1768d5de891
A-Fayed <a_fayed@outlook.com>
2018-08-26 02:59:19 +0200

    add post method settings and json stringfy spacer

--- api.js
+++ api.js
@@ -22,10 +22,11 @@ function createForm (){
 	for (i=0; i< Object.keys(ApiRespond).length; i++){
 		var propertyName = Object.keys(ApiRespond)[i];
 		var propertyType = ApiRespond[propertyName].type;
+		var propertyValue = ApiRespond[propertyName].value;
 
 		$('form').append('<p>');
 			if ( propertyName ==='secret' || propertyName ==='submit' ) {
-				$('p').last().append($('<label>').text(propertyName).attr({ 'class': 'visually-hidden', 'for': propertyName}));
+				$('p').last().append($('<label>').text(propertyName).attr({ 'class': 'visually-hidden', 'for': propertyName, 'value': propertyValue}));
 				$('p').last().append($('<input>').attr({'type': propertyType, 'name': propertyName, 'id':propertyName}));	
 			} else {
 				$('p').last().append($('<label>').text(propertyName).attr('for', propertyName));
@@ -45,31 +46,39 @@ function createResponse (){
 	for (i=0; i<myinput.length; i++) {
 		if (ApiRespond[Object.keys(ApiRespond)[i]].type === 'checkbox'){
 			myinput[i].value = myinput[i].checked;
+		} else if (ApiRespond[Object.keys(ApiRespond)[i]].type === 'hidden'){
+			myinput[i].value = ApiRespond[Object.keys(ApiRespond)[i]].value;
 		}
-
 	obj[Object.keys(ApiRespond)[i]] = myinput[i].value;
 	};
-	delete obj["secret"];
-	delete obj["submit"];
+
 };
 
 // ----- POSTING AS JSON RESPONSE -----------------------------------------------------------------------------
 
+
+
 $('.API').on('submit','form', function(event){
 	event.preventDefault();
 	createResponse();
+	var finalResponse = JSON.stringify(obj, null, '\t') ;
+	console.log(finalResponse);
 
-	$.ajax({ url: 'https://levelup-assessment-backend-odvoreherl.now.sh/api/submission' ,
-		method: 'POST',
-		data: JSON.stringify(obj),
-		dataType: 'json',
-		success:function(data){
-			console.log(data);
-		},
+	var settings = {
+  "async": true,
+  "crossDomain": true,
+  // "url": ""https://levelup-assessment-backend-odvoreherl.now.sh/api/submission",
+  "method": "POST",
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "processData": false,
+  "data": finalResponse
+}
 
-		error:function(error){
-			console.log(error);
-		},
-	});
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
 });
 
